895765ae0fcdbf88b4ff14c61254140eaf439ffe
Thetail001 <56257172+Thetail001@users.noreply.github.com>
2025-02-06 13:18:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -79,6 +79,7 @@ class ImageSearchPlugin(BasePlugin):
         try:
             async with Network() as client:
                 yandex = Yandex(client=client)
+                self.ap.logger.info(f"temp_file_path： {temp_file_path}")      
                 resp = await yandex.search(file=temp_image_path)
                 return self.parse_result(resp)
         except Exception as e:
