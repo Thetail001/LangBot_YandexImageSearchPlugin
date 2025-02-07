17f8fd58db8ec4643337ce6e00d8ae9f032b24df
Thetail001 <56257172+Thetail001@users.noreply.github.com>
2025-02-07 22:52:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ class ImageSearchPlugin(BasePlugin):
         message_chain = ctx.event.query.message_chain
         for message in message_chain:
             if isinstance(message, platform_types.Image):
-                self.ap.logger.info(f"图片消息数据: {json.dumps(message.__dict__, default=str, indent=4)}")
+                self.ap.logger.info(f"图片消息数据: {message}")
                 if message.base64:
                     temp_image_path = self.save_base64_image(message.base64)
                     try:
